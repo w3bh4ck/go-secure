3edb0ce0082efb80d971dab368cf125b685e19d0
Lucky Amadi <lucky@softcom.ng>
2019-02-14 17:38:26 +0100

    added the navbar

---
+++ src/components/layout/Layout.js
@@ -0,0 +1,15 @@
+import React, { Component } from 'react'
+import Navbar from '../navbar/Navbar';
+
+class Layout extends Component {
+    render() {
+        return (
+            <div className="layout">
+                <Navbar />
+            </div>
+        )
+    }
+}
+
+
+export default Layout;
---
+++ src/components/navbar/Navbar.js
@@ -0,0 +1,22 @@
+import React from 'react'
+
+const Navbar = () => {
+    return (
+        <nav className="navbar navbar-expand-lg navbar-light" style={{ height: "3rem", backgroundColor: "gray" }} >
+            <a className="navbar-brand" href="#">Go Secure</a>
+            <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNavAltMarkup" aria-controls="navbarNavAltMarkup" aria-expanded="false" aria-label="Toggle navigation">
+                <span className="navbar-toggler-icon"></span>
+            </button>
+            <div className="collapse navbar-collapse" id="navbarNavAltMarkup">
+                <div className="navbar-nav">
+                    <a className="nav-item nav-link active" href="#">Home <span className="sr-only">(current)</span></a>
+                    <a className="nav-item nav-link" href="#">Features</a>
+                    <a className="nav-item nav-link" href="#">Pricing</a>
+                    <a className="nav-item nav-link disabled" href="#" tabindex="-1" aria-disabled="true">Disabled</a>
+                </div>
+            </div>
+        </nav>
+    )
+}
+
+export default Navbar;
